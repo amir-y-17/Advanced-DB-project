b59de7a2c938b64f4775814ed8c791cb961a6f12
Amir <am17iryaghoobi@gmail.com>
2025-12-22 03:55:10 +0330

    Add accounts management endpoints

---
+++ accounts/urls.py
@@ -0,0 +1,21 @@
+from . import views
+from django.urls import path
+
+
+app_name = "accounts"
+urlpatterns = [
+    # path("users/admin/", views.CreateAdminView.as_view(), name="create-admin"),
+    path("users/", views.UserListView.as_view(), name="user-list"),
+    path(
+        "users/<int:pk>/",
+        views.UserUpdateDetailView.as_view(),
+        name="user-detail-update",
+    ),
+    path("token/", views.LoginView.as_view(), name="token_obtain_pair"),
+    path("refresh-token/", views.RefreshTokenView.as_view(), name="token_refresh"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("refresh-token/", views.RefreshTokenView.as_view(), name="refresh-token"),
+    path(
+        "change-password/", views.ChangePasswordView.as_view(), name="change-password"
+    ),
+]
